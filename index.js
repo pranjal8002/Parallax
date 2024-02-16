4c630dc53ba7c09c91b23b4512db9adbc2892679
pranjal8002 <157972443+pranjal8002@users.noreply.github.com>
2024-02-16 23:08:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener(("scroll"),()=>{
+    document.querySelector("#Home h1").style.marginTop = `${window.scrollY * 1.5}px`
+    document.querySelector("#Mountain1").style.marginBottom = `${106 - window.scrollY}px`
+    document.querySelector("#leftCloud").style.marginLeft = `-${window.scrollY}px`
+    document.querySelector("#mainCloud").style.marginTop = `-${window.scrollY}px`
+    document.querySelector("#rightCloud").style.marginRight = `-${window.scrollY}px`
+})
+const aboutPage = document.querySelector('.about-page');
+aboutPage.addEventListener('load', () => {
+  document.body.classList.add('about-page-loaded');
+});
